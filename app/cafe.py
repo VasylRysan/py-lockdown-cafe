2e667520d4685842bb59704646b40136ebe56f92
vasyl.rysan <Widbmak@gmail.com>
2025-07-21 13:09:08 +0300

    sol2

--- app/cafe.py
+++ app/cafe.py
@@ -14,12 +14,12 @@ class Cafe:
         if "vaccine" in visitor:
             exp_date = visitor.get("vaccine").get("expiration_date")
             if exp_date < datetime.date.today():
-                raise OutdatedVaccineError(" ")
+                raise OutdatedVaccineError("Your vaccine is expired!")
         else:
-            raise NotVaccinatedError(" ")
+            raise NotVaccinatedError("You should be vaccinated!")
 
         visitor_has_mask = visitor.get("wearing_a_mask", False)
         if visitor_has_mask:
             return f"Welcome to {self.name}"
         else:
-            raise NotWearingMaskError(" ")
+            raise NotWearingMaskError("You should wear a mask!")
